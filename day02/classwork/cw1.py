69835f6e0770575660e3ff5e83203df3aeec2c10
SISICHAA <arabulisaba699@gmail.com>
2025-07-05 23:05:10 +0400

    1

--- day02/classwork/cw1.py
+++ day02/classwork/cw1.py
@@ -1,1 +1,78 @@
-#ss
+#დღეს ჩვენ გავიარეთ მათემატიკური მაგალითები მიმატება გამოკლება გაყოფა და გამმრრავლება
+
+
+#სტრინგისა და ინტეჯერის მიმატება არშეილება result ---->error
+
+
+
+# print(10 + 5)
+
+
+
+
+# print(50 - 43)
+
+
+# print(5 * 3)
+
+
+# print(40 / 8)
+
+
+
+# name = "saba"
+
+
+# print(name)
+
+
+# num1 = 20
+
+
+
+
+# name = "mayvala"
+
+
+
+# print(str(num1) + name)
+
+
+
+# num1 = 5
+
+# name = "saba"
+
+
+# print(num1 * name)
+
+
+# name = "jora"
+
+# name1 = "jambuli"
+
+# print(name + " " + name1)
+
+
+num1 = 40
+
+
+num2 = 130
+
+
+num3 = num1 + num2
+
+print(num3)
+
+num1 = 20
+
+
+
+
+
+
+
+
+
+
+
