838db6b7b8195a8df288a7543ba5e93c30ea4e1d
SISICHAA <arabulisaba699@gmail.com>
2025-07-08 23:01:56 +0400

    1

--- day03/classwork/cw2.py
+++ day03/classwork/cw2.py
@@ -1,58 +1,58 @@
-num1 = 100
+# num1 = 100
 
-num1 = 69
+# num1 = 69
 
-print(num1)
-
-
-Brand = "Mercedes"
-brand = "Opel"
-
-BRAND = "BMW"
-BRand = "Toyota"
-BRANd = "Lada"
-
-nikas_age = 15
-nikas_age = 17
-print(nikas_age)
-
-snake_case
+# print(num1)
 
 
+# Brand = "Mercedes"
+# brand = "Opel"
 
-user_name1 = "saba"
-user_name2 = "mate"
-user_name3 = "gio"
+# BRAND = "BMW"
+# BRand = "Toyota"
+# BRANd = "Lada"
 
-user_name4 = "nika"
+# nikas_age = 15
+# nikas_age = 17
+# print(nikas_age)
 
-print(user_name1)
-
-print(user_name2)
-print(user_name3)
-print(user_name4)
-
-
-market_price1 = 5.6
-market_price2 = 5.5
-market_price3 = 8.6
-market_price4 = 77.9
-
-print(market_price1)
-print(market_price2)
-print(market_price3)
-print(market_price4)
+# snake_case
 
 
-username_age1 = 15
-username_age2 = 23
-username_age3 = 53
-username_age4 = 68
 
-print(username_age1)
-print(username_age2)
-print(username_age3)
-print(username_age4)
+# user_name1 = "saba"
+# user_name2 = "mate"
+# user_name3 = "gio"
+
+# user_name4 = "nika"
+
+# print(user_name1)
+
+# print(user_name2)
+# print(user_name3)
+# print(user_name4)
+
+
+# market_price1 = 5.6
+# market_price2 = 5.5
+# market_price3 = 8.6
+# market_price4 = 77.9
+
+# print(market_price1)
+# print(market_price2)
+# print(market_price3)
+# print(market_price4)
+
+
+# username_age1 = 15
+# username_age2 = 23
+# username_age3 = 53
+# username_age4 = 68
+
+# print(username_age1)
+# print(username_age2)
+# print(username_age3)
+# print(username_age4)
 
 
 
