c6baad3bf247ed8abe1a2cb459293f49ef50c82e
SISICHAA <arabulisaba699@gmail.com>
2025-07-12 23:10:35 +0400

    1

--- day04/classwork/cw1.py
+++ day04/classwork/cw1.py
@@ -1,3 +1,28 @@
-#dasdasd
+#data type ------> string  #
+# integer  
+# float 
+# boolean
+
+
+age = input("Enter your age:  ")
+height = input( "Enter your height:")
+
+print(int(age) + int(height))
+
+
+age = input("Enter your age:   ")
+
+print(int(age))
+
+
+
+brand = "mercedes"
+num1 = 13
+price = 3.45
+homework = True
+print(brand)
+print(num1)
+print(price)
+print(homework)
 
 
--- day04/homework/hw1.py
+++ day04/homework/hw1.py
@@ -1,1 +1,1 @@
-#rdsd
+#rdsdj
