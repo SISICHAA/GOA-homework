b66e78c8881fee5f791affcb6bc3632642f7862b
SISICHAA <arabulisaba699@gmail.com>
2025-07-15 23:01:31 +0400

    1

---
+++ day05/classwork/cw1.py
@@ -0,0 +1,29 @@
+#შედარების ოპერაციები
+
+
+#  >    , <  , ==
+ 
+# print(10>5)
+
+
+# print(10<3)
+# print(14==43)
+
+
+# #მინიჭების ოპერატორი =
+# num1 = 20
+
+
+# first = int(input("enter first number: "))
+# second = int(input("enter second number: "))
+
+# print(first > second)
+# print(first < second)
+# print(first == second)  
+
+#logical operators
+
+
+#and and or
+    
+
