048323b148f884d0323c389776db946fc3666b92
SISICHAA <arabulisaba699@gmail.com>
2025-07-18 15:34:49 +0400

    1

--- day05/homework/hw1.py
+++ day05/homework/hw1.py
@@ -1,1 +1,69 @@
-#dsadds
+#ჩვენ ვისწავლეთ შემდეგი ოპერატორები : < > =
+# > ეს ნისჰნავს რომ რაგაცა რაგაცაზე მეტია მაგალითად 5>3
+#<ეს ნიშნავს რომ რაგაცა რაგაცაზე ნაკლებია მაგალითად : 5<34
+#ასევე = რომელიც ცვლადს ანიჭებს რაგაცა მნიშვნელობას მაგალითად : num1 = 14
+
+# age = int(input("enter first age:  "))
+# age1 = int(input("enter second age: "))
+
+# print(age > age1)
+# print(age < age1)
+# print(age == age1)
+
+
+
+# name1 = str(input("enter your first name please:  "))
+# name2 = str(input("enter your second name please:  "))
+# name3 = str(input("enter your third name please:  "))
+# name4 = str(input("enter your fourth name please:  "))
+# name5 = str(input("enter your fifth name please:  "))
+
+# overal = name1 + name2 + name3 + name4 + name5
+
+# print(overal)
+
+
+# num1 = int(input("enter first number:  "))
+# num2 = int(input("enter second number:  "))
+# num3 = int(input("enter third number:  "))
+# num4 = int(input("enter fourth number:  "))
+
+
+# overal1 = (num1 + num2 + num3 + num4)/ 4
+# print(overal1)
+
+# name = "saba"
+# age = 14
+# price = 4.54
+# math = 5<2
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type( math))
+
+# country1 = "georgia"
+# country2 = "france"
+# print(country1 > country2)
+
+
+# number = "13"
+# number1 = "14"
+# number2 = "142"
+# number3 = "98"
+# overal = int(number) + int(number1) + int(number2) + int(number3)
+# print(overal)
+
+
+age1 = 14
+age2 = 15
+age3 = 16
+age4 = 17
+
+overal = str(age1) + str(age2) + str(age3) + str(age4)
+
+
+print(overal)
+
+
+
+
