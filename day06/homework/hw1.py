edd8da6ec6467ebc0417244518eca92f53138223
SISICHAA <arabulisaba699@gmail.com>
2025-07-28 21:01:47 +0400

    1

---
+++ day06/homework/hw1.py
@@ -0,0 +1,64 @@
+
+# age = int(input("please enter your age: " ))
+# agE = int(input("please enter your age: " ))
+
+# overal1 = age > agE
+# overal2 = age < agE
+# overal3 = age == agE
+
+# name1 = "35"
+# name2 = "34"
+# name3 = "56"
+
+# age1 = 13
+# age2 = 14
+# overall = int(name1) * int(name2) * name3 * age1 * age2
+# print(overall)
+
+town3 = input("enter your town please: ")
+town2 = input("enter your town please: ")
+town1 = input("enter your town please: ")
+int1 = int(input("enter your number please: "))
+word_sum = town1 + town2 + town3
+
+print(word_sum * int1)
+
+
+
+#თუ წინადადებასი არის or და ერთერთი არის true მაშინ წინადება true false იქნება მხოლოდნიმშემთხვევაში თუ ორიე კომპონენტი ფალსია
+#თუ წინადადებაში არის and და ერთერთი არის false მაშინ წინადადება არის false true იქნება მოხლოდ იმ შემთხვევაში თუ ორივე არის თრუ
+
+
+#პირველი გამოიტანს trues imitpm rom orive aris true
+#meore gamoitans falses imitoro orive aris false
+#mesame gamoitans false imitoro erti aris false
+#meotxe gamoitans false imitoro orive aris false
+
+print(True and False)
+print(False and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False and True)
+print(False and False)
+
+price = 1.32
+name = "saba"
+height = 13
+bool = True
+
+print(type(price))
+print(type(name))
+print(type(height))
+print(type(bool))
+
+
+
+price = float(input("enter price :"))
+int = int(input("enter number"))
+str = str(input("enter name"))
+
+
+
+
