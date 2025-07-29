dd74f9e008ff3d8a32b1a900339f43dc7488986d
SISICHAA <arabulisaba699@gmail.com>
2025-07-29 11:36:28 +0400

    1

--- day08/homework/hw1.py
+++ day08/homework/hw1.py
@@ -1,38 +1,90 @@
-# print(50>23 or 20==42)
-# print(21<42) or 10<3
-# print(40==23 or 23<24)
-# print(1334>1222 and 32==32)
-# print(40==40 or 0==3)
+# # print(50>23 or 20==42)
+# # print(21<42) or 10<3
+# # print(40==23 or 23<24)
+# # print(1334>1222 and 32==32)
+# # print(40==40 or 0==3)
 
 
-# print(13<32 and 40==42)
-# print(13==32 and 40>23)
-# print(132<1444 and 1==4)
-# print(13<23 and 200==233)
-# print(13>23 or 0==1)
-
-
-
+# # print(13<32 and 40==42)
+# # print(13==32 and 40>23)
+# # print(132<1444 and 1==4)
+# # print(13<23 and 200==233)
+# # print(13>23 or 0==1)
 
 
 
-# #sequancing - წერა ზევიდან ქვევით
-# #interaction - გამეორება ან გარკვეული რაოდენობით ან უსასრულოდ
-# #selection - არჩევანი 
-
-
-# print("nika")
-
-# print("1.32")
-# print("132")
 
 
 
-# #ეს არის sequancing მაგალითი რადგან ეს რომ გავუშვათ ტერმინალში ჩვენ მივიგებთ თანმიმდევრობით
+# # #sequancing - წერა ზევიდან ქვევით
+# # #interaction - გამეორება ან გარკვეული რაოდენობით ან უსასრულოდ
+# # #selection - არჩევანი 
+
+
+# # print("nika")
+
+# # print("1.32")
+# # print("132")
 
 
 
-#for loop ჩვენ გვეხმარება გავიმეოროთ ესა თუ ის იმდენჯერ რამდენჯერაც გვინდა ხელიტ წერის ნაცვლად
-i = "saba"
-for i in range(10):
-    print(i)
+# # #ეს არის sequancing მაგალითი რადგან ეს რომ გავუშვათ ტერმინალში ჩვენ მივიგებთ თანმიმდევრობით
+
+
+
+# #for loop ჩვენ გვეხმარება გავიმეოროთ ესა თუ ის იმდენჯერ რამდენჯერაც გვინდა ხელიტ წერის ნაცვლად
+# i = "saba"
+# for i in range(10):
+#     print(i)
+
+
+# #ranges gadaecema tu ramdenjer gaaameoros esa tu is 
+# #xolo for loop gameorebashi xolo for loop vwert ramdenjerac gvinda gamoreba
+
+# for i in range(10):
+#     print("ჩემი საყვარელი მანქანაა m5")
+
+
+
+
+# for i in range(100):
+#     print("arabuli")
+
+
+# for i in range(46):
+#     print("mwvane")
+
+
+
+
+# for i in range(32):
+#     print("s")
+
+
+
+
+
+
+name1 = str(input("name please:  ")) 
+name2 = str(input("name please:  "))
+name3 = str(input("name please:  "))
+
+age1 = str(input("age please: "))
+
+overal = name1 + name2 + name3 + age1
+print(overal)
+
+
+
+
+height = 1.43
+age = 13
+name = "saba"
+bool = True
+
+print(type(height))
+print(type(age))
+print(type(name))
+print(type(bool))
+
+
