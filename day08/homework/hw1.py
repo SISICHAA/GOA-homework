a4c9dbb2eb1ad03c6ab7be74e2f62913533348b7
SISICHAA <arabulisaba699@gmail.com>
2025-07-29 12:25:01 +0400

    1

--- day08/homework/hw1.py
+++ day08/homework/hw1.py
@@ -1,64 +1,64 @@
-# # print(50>23 or 20==42)
-# # print(21<42) or 10<3
-# # print(40==23 or 23<24)
-# # print(1334>1222 and 32==32)
-# # print(40==40 or 0==3)
+print(50>23 or 20==42)
+print(21<42) or 10<3
+print(40==23 or 23<24)
+print(1334>1222 and 32==32)
+print(40==40 or 0==3)
 
 
-# # print(13<32 and 40==42)
-# # print(13==32 and 40>23)
-# # print(132<1444 and 1==4)
-# # print(13<23 and 200==233)
-# # print(13>23 or 0==1)
-
-
-
-
-
-
-# # #sequancing - წერა ზევიდან ქვევით
-# # #interaction - გამეორება ან გარკვეული რაოდენობით ან უსასრულოდ
-# # #selection - არჩევანი 
-
-
-# # print("nika")
-
-# # print("1.32")
-# # print("132")
-
-
-
-# # #ეს არის sequancing მაგალითი რადგან ეს რომ გავუშვათ ტერმინალში ჩვენ მივიგებთ თანმიმდევრობით
-
-
-
-# #for loop ჩვენ გვეხმარება გავიმეოროთ ესა თუ ის იმდენჯერ რამდენჯერაც გვინდა ხელიტ წერის ნაცვლად
-# i = "saba"
-# for i in range(10):
-#     print(i)
-
-
-# #ranges gadaecema tu ramdenjer gaaameoros esa tu is 
-# #xolo for loop gameorebashi xolo for loop vwert ramdenjerac gvinda gamoreba
-
-# for i in range(10):
-#     print("ჩემი საყვარელი მანქანაა m5")
+print(13<32 and 40==42)
+print(13==32 and 40>23)
+print(132<1444 and 1==4)
+print(13<23 and 200==233)
+print(13>23 or 0==1)
 
 
 
 
-# for i in range(100):
-#     print("arabuli")
 
 
-# for i in range(46):
-#     print("mwvane")
+# #sequancing - წერა ზევიდან ქვევით
+# #interaction - გამეორება ან გარკვეული რაოდენობით ან უსასრულოდ
+# #selection - არჩევანი 
+
+
+print("nika")
+
+print("1.32")
+print("132")
+
+
+
+# #ეს არის sequancing მაგალითი რადგან ეს რომ გავუშვათ ტერმინალში ჩვენ მივიგებთ თანმიმდევრობით
+
+
+
+#for loop ჩვენ გვეხმარება გავიმეოროთ ესა თუ ის იმდენჯერ რამდენჯერაც გვინდა ხელიტ წერის ნაცვლად
+i = "saba"
+for i in range(10):
+    print(i)
+
+
+#ranges gadaecema tu ramdenjer gaaameoros esa tu is 
+#xolo for loop gameorebashi xolo for loop vwert ramdenjerac gvinda gamoreba
+
+for i in range(10):
+    print("ჩემი საყვარელი მანქანაა m5")
 
 
 
 
-# for i in range(32):
-#     print("s")
+for i in range(100):
+    print("arabuli")
+
+
+for i in range(46):
+    print("mwvane")
+
+
+
+
+for i in range(32):
+    print("s")
 
 
 
@@ -86,5 +86,14 @@ print(type(height))
 print(type(age))
 print(type(name))
 print(type(bool))
+
+
+num1 = int(input("number"))
+num2 = int(input("number"))
+num3 = int(input("number"))
+num4 = int(input("number"))
+
+overal = num1 + num2 + num3 + num4
+print(overal)
 
 
