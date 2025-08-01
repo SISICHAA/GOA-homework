eb0075d7780c2f33307f81007bb609ce5fbafad8
SISICHAA <arabulisaba699@gmail.com>
2025-08-01 14:55:25 +0400

    1

--- day06/classwork/cw1.py
+++ day06/classwork/cw1.py
@@ -1,3 +1,15 @@
-print("azma")
+#ლოგიკური ოპერატორები გამოიყენება კოდის ლოგიკურად გასწორებაში ის შეიცავს and or 
+
+name = str(input("enter your name please"))
+print(name == "saba")
+
+
+print(40 == 30 and 22 == 22)
+print(233 == 22 and 32 == 32)
+
+print(233 == 2332 or 90 == 90)
+print(2123 == 12332 or 40>23)
+
+
 
 
