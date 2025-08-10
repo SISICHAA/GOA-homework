e71216c53005013382042bb70affcebcff56505d
SISICHAA <arabulisaba699@gmail.com>
2025-08-10 12:48:22 +0400

    1

---
+++ day12/classwork/cw1.py
@@ -0,0 +1,12 @@
+for i in range(11):
+    print(i)
+i = 5
+while i < 20:
+    print(i)
+    i = i + 1
+
+age = int(input("enter ypur age please"))
+if age < 18:
+    print("you are teen")
+else:
+    print("you are adult")
