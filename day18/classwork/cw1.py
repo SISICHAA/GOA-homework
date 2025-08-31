4ff88cea643b6edcf73010e5ab0030fd041a6d0b
SISICHAA <arabulisaba699@gmail.com>
2025-08-31 20:48:50 +0400

    1

---
+++ day18/classwork/cw1.py
@@ -0,0 +1,66 @@
+num1 = str(input("enter number please "))
+num2 = str(input("enter number please "))
+num3 = str(input("enter number please "))
+
+if num1 == num3:
+    print(f"{num1} = {num3}")
+elif num1 == num2:
+    print(f"{num1} = {num2}")
+elif num2 == num3:
+    print(f"{num2} = {num3}")
+else:
+    print("wrong input")
+
+
+number = int(input("enter number though 1 - 12"))
+
+
+if number ==12:
+    print("zamtari")
+elif number == 1:
+    print("zamtari")
+elif number == 2:
+    print("zamtari")
+
+
+
+if number == 3:
+    print("gazafxuli")
+elif number == 4:
+    print("gazafxuli")
+elif number == 5:
+    print("gazafxuli")    
+
+
+
+elif number == 6:
+
+    print("zafxuli")
+
+elif number == 7:
+    print("zafxuli")
+elif number == 8:
+    print("zafxuli")
+elif number == 9:
+    print("daprintet")
+elif number == 10:
+    print("daprintet")
+elif number == 11:
+    print("daprintet")
+
+
+else:
+    print("wrong input")
+
+
+name = str(input("enter your name please"))
+
+password = str("enter your password")
+
+
+if name == "admin:":
+    print(f"{password}")
+elif password == "adminpasword123:":
+    print("salami admin")
+else:
+    print("salami momxmarebelo")
