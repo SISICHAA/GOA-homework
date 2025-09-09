222cb9c736ae517ea00a59fdfb9ca5a297160233
SISICHAA <arabulisaba699@gmail.com>
2025-09-09 23:05:00 +0400

    1

--- day20/classwork/cw1.py
+++ day20/classwork/cw1.py
@@ -1,1 +1,14 @@
-#sad
+#chven dges viswavlet 3 matematikuri operatori esenia // ** da % aqedan // gvanaxebs tu ramdenjer  chaeteva meore ricvxi pirvel ricxvshi magalitad
+print(10 // 3)
+print(66 // 34)
+print(123 // 32)
+# ** aris xarisxi magalitad
+print(3 ** 3)
+print(12 **2)
+print(23 ** 7)
+# % xolo sabolood es nishnavs nashts magalitad
+print(324 % 231)
+print(3423 % 123)
+print(123 % 23)
+
+#siebi da cvladebi gansxvavdebian cvladebshi shegvidzlia marto erti mnishvnelobis shenaxva xolo siebshi uamravis
