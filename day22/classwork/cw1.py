a3c7d1d9f43678d9afde2daa445fa53b0f57cbda
SISICHAA <arabulisaba699@gmail.com>
2025-09-13 22:53:27 +0400

    1

---
+++ day22/classwork/cw1.py
@@ -0,0 +1,22 @@
+list = ["saba", "gio", "lasha", "zviko", "mate" , "oto" , "makvala" , "goga" , "maro" , "zviada"]
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[8])
+print(list[9])
+
+
+listt = ["kitri" , "pomidori", "kartofili", "xaxvi", "niori" , "brokoli"]
+
+listt[1] = "viri"
+listt[3] = "tagvit"
+listt[5] = "bokveri"
+print(listt[1])
+print(listt[3])
+print(listt[5])
