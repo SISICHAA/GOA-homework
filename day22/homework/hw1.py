cdffc15bc1873286b91c5a1f11b80e3e37d9b88c
SISICHAA <arabulisaba699@gmail.com>
2025-09-16 21:04:02 +0400

    1

---
+++ day22/homework/hw1.py
@@ -0,0 +1,8 @@
+#indexi aris listshi shetanili moonacemis misamarti 
+math =  [4,6,1,5,9,8,4]
+print(math[1] + math[0])
+print(math[2] + math[2])
+print(math[3] + math[4])
+print(math[3] * math[0])
+print(math[5] / math[0])
+print(math[0])
