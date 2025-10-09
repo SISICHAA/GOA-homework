4f5e25a1afdf69d72f167dca77ac9338926736aa
SISICHAA <arabulisaba699@gmail.com>
2025-10-09 21:10:14 +0400

    1

---
+++ day29/homework/hw1.py
@@ -0,0 +1,53 @@
+word = "Pythonprogramming"
+
+print(word[0 : 6])
+
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[2 : 5 ])
+
+print(numbers[-5 : -2])
+
+
+hh = "helloword"
+
+
+print(hh[0 : 5])
+
+
+print(hh[-9 : -4])
+
+
+ww = "Information"
+
+print(ww[2 : 7])
+
+
+print(ww[-9 : -4])
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+
+print(letters[0])  
+
+print(letters[2])   
+
+
+print(letters[4])   
+
+print(letters[-7])  
+
+print(letters[-5]) 
+
+print(letters[-3])  
+
+
+
+word = "abcdefghijklmno"
+
+
+print(word[0 : 4])
+
+print(word[9: ])
+
+
+print(word[5: 10])
