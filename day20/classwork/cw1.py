cb50d9c0d738a9ce5eb28f1914d5cb5f7bf3bc75
SISICHAA <arabulisaba699@gmail.com>
2026-01-15 23:18:54 +0400

    1

---
+++ day20/classwork/cw1.py
@@ -0,0 +1,17 @@
+#დღეს ჩვენ ვისწავლეთ სამი ოპერატორი ესენია ** // % აქედან ** nishnavs xarisxs
+print(10 ** 9)
+print(233 ** 32)
+print(13 **4)
+#შემდეგი გახლავთ // ეს ნიშნავს თუ რამდენჯერ ჩაეტევა მეორე რიცხვი პირველში
+print(100 // 23)
+print(123 //3)
+print(554 // 54)
+#ბოლოს გვაქ % ეს გვიჩვენებს თუ რამდენი დარჩება ნაშთი მაგალითად
+print(443 % 42)
+print(655 % 23)
+print(43 % 23)
+
+#siebi da cvladebi gansxvavdebian radgan cvkadshi hseileba mxolod erti ramis shenaxva xolo listshi uamravis
+#siebs viyenebt bevri informaciis ertad damushavebistvis
+list = [True , 32 , 32.332 , "sava"]
+print(list)
