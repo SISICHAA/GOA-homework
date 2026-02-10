34534638e2f6559a8ddd332b065635ce5366157b
SISICHAA <arabulisaba699@gmail.com>
2026-02-10 19:17:10 +0400

    1

---
+++ day64/classwork/cw1.py
@@ -0,0 +1,19 @@
+
+def say_my_name():
+    name = input("enter your name")
+    print("your name is", name)
+say_my_name()
+say_my_name()
+say_my_name()
+say_my_name()
+
+
+def check_even():
+    num = 19
+    if num % 2 == 0:
+        return "es ricxvi luwia"
+    else:
+        return "es ricxvi kentia"
+    
+print(check_even())
+    
